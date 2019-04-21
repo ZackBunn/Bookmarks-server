4a6adee0eae294d3a2af116a84f73ebf17200d0a
Zack Bunn <zrb6578@gmail.com>
2019-04-20 21:00:33 -0500

    sanitized scripts

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    API_TOKEN: process.env.API_TOKEN || 'b35b9b60-dd4b-4910-b002-91ed66f8944a',
+    API_TOKEN: process.env.API_TOKEN || 'dummy-api-token',
     DB_URL: process.env.DB_URL || 'postgresql://dunder-mifflin@localhost/bookmarks',
   }
